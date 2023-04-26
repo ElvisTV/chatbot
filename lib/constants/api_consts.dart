df48b61c549759618d38e6195eb127a77c98323b
elvis arturo tezen villanueva <elvistezen@outlook.com>
2023-04-26 10:13:06 -0500

    hasta una hora con 8 minutos

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -6,40 +6,36 @@ import '../constants/constants.dart';
 import '../widgets/text_widget.dart';
 
 class Services {
+  static Future<void> showModalSheet({required BuildContext context}) async {
 
-static Future<void> showModalSheet({required BuildContext context}) async {
-
-  await showModalBottomSheet(
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-        top: Radius.circular(20)
-      )
-    ) ,
-    backgroundColor: scaffoldBackground,
-    context: context,
-    builder: (context) {
-      return Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Flexible(
-              child: TextWidget(
-                label: "Chosen Model:",
-                fontSize: 16,
+    await showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(20)
+        )
+      ) ,
+      backgroundColor: scaffoldBackground,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Flexible(
+                child: TextWidget(
+                  label: "Chosen Model:",
+                  fontSize: 16,
+                ),
               ),
-            ),
-            Flexible(
-              flex: 2,
-              child: ModelsDrowDownWidget()
-            ),
-          ],
-        ),
-      );
-    }
-  );
-
-}
-
-
+              Flexible(
+                flex: 2,
+                child: ModelsDrowDownWidget()
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
 }
